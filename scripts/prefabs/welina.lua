e498f12f274e9d07c3f0d880cc1cae2ebb7bf574
Cliffford W <55302963+ClifffordW@users.noreply.github.com>
2024-02-11 20:52:10 +0100

    Update welina.lua

--- scripts/prefabs/welina.lua
+++ scripts/prefabs/welina.lua
@@ -28,6 +28,90 @@ local function CLIENT_Welina_HostileTest(inst, target)
 end
 
 
+local PROX_CHECK_TAGS = {"player", "_follower"}
+local PROX_CANT_TAGS = {"emocatgirl"}
+
+function GetFollowerPenalty(inst, max)
+
+	
+
+	local x,y,z = inst.Transform:GetWorldPosition()
+
+	local asocial = TheSim:FindEntities(x, y, z, 6, nil, PROX_CANT_TAGS, PROX_CHECK_TAGS)
+	local asocial_followers = #asocial
+
+	local modifiername = 1 - asocial_followers * 0.25
+	modifiername = math.max(modifiername, max or 0.95)
+
+
+	return modifiername
+
+end
+
+
+
+
+local function AsocialWork(inst, data)
+
+		local workModifier = GetFollowerPenalty(inst, 0.25)
+		
+		inst.components.workmultiplier:AddMultiplier(ACTIONS.CHOP,   workModifier, inst)
+		inst.components.workmultiplier:AddMultiplier(ACTIONS.MINE,   workModifier, inst)
+		inst.components.workmultiplier:AddMultiplier(ACTIONS.HAMMER, workModifier, inst)
+		print(workModifier)
+
+
+
+end
+
+
+local function SanityScrew(inst)
+	local sanityModifier = GetFollowerPenalty(inst, -10)
+
+
+
+	inst.components.sanity.dapperness = sanityModifier
+
+	
+	--print(sanityModifier)
+	
+end
+
+
+
+
+
+
+local function OnDeath(inst)
+    inst.numDeaths = inst.numDeaths + 1  
+    if inst.numDeaths >= 9 then
+        inst.components.health.canrevive = false
+
+    end
+end
+
+
+
+function OnSave(inst, data)
+    
+    data.numDeaths = inst.numDeaths
+	print(data.numDeaths)
+    
+end
+
+function OnLoad(inst, data)
+    if data and data.numDeaths then
+		print(data.numDeaths)
+        inst.numDeaths = data.numDeaths
+        if inst.numDeaths >= 9 then
+            inst.components.health.canrevive = false
+        end
+    end
+end
+
+
+
+
 
 -- This initializes for both the server and client. Tags can be added here.
 
@@ -54,6 +138,7 @@ local master_postinit = function(inst)
 	inst.talker_path_override = "scotchmintz_characters/characters/"
 	inst.soundsname = "welina"
 
+	
 
 	-- Stats	
 	inst.components.health:SetMaxHealth(TUNING.WELINA_HEALTH)
@@ -74,6 +159,14 @@ local master_postinit = function(inst)
 	
 	inst.components.locomotor:SetExternalSpeedMultiplier(inst, "welina_speed_mod", TUNING.WELINA_MOVESPEED)
 
+	inst:ListenForEvent("working", AsocialWork)
+	inst:ListenForEvent("sanitydelta", SanityScrew)
+	inst:ListenForEvent("death", OnDeath)
+
+	inst.OnSave = OnSave
+	inst.OnLoad = OnLoad
+
+	inst.numDeaths = inst.numDeaths or 0
 
 
 	end
