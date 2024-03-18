533740a65ba3695677a0a2440fad8a58e408bcc2
Cliffford W <55302963+ClifffordW@users.noreply.github.com>
2024-03-18 14:43:25 +0100

    Update welina_stategraphs.lua

--- init/welina_stategraphs.lua
+++ init/welina_stategraphs.lua
@@ -1,3 +1,27 @@
+local function IsChannelCasting(inst)
+	--essentially prediction, since the actions aren't busy w/ lag states
+	local buffaction = inst.sg.mem.preview_channelcast_action
+	if buffaction then
+		return buffaction.action == ACTIONS.START_CHANNELCAST
+		--Don't use "or inst:IsChannelCasting()"
+		--We want to be able to return false here when predicting!
+	end
+	--otherwise return server state
+	return inst:IsChannelCasting()
+end
+
+local function IsChannelCastingItem(inst)
+	--essentially prediction, since the actions aren't busy w/ lag states
+	local buffaction = inst.sg.mem.preview_channelcast_action
+	if buffaction then
+		return buffaction.invobject ~= nil
+		--Don't use "or inst:IsChannelCastingItem()"
+		--We want to be able to return false here when predicting!
+	end
+	--otherwise return server state
+	return inst:IsChannelCastingItem()
+end
+
 local function ConfigureRunState(inst)
     if inst.components.rider and inst.components.rider:IsRiding() then
         inst.sg.statemem.riding = true
@@ -72,6 +96,19 @@ local function DoFoleySounds(inst)
         inst.SoundEmitter:PlaySound(inst.foleysound, nil, nil, true)
     end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 AddStategraphState("wilson",
     State {
@@ -212,10 +249,97 @@ AddStategraphState("wilson", State {
 
 
 
+local function ConfigureRunState(inst)
+    if inst.replica.rider ~= nil and inst.replica.rider:IsRiding() then
+        inst.sg.statemem.riding = true
+        inst.sg.statemem.groggy = inst:HasTag("groggy")
+
+        local mount = inst.replica.rider:GetMount()
+        inst.sg.statemem.ridingwoby = mount and mount:HasTag("woby")
+
+    elseif inst.replica.inventory:IsHeavyLifting() then
+        inst.sg.statemem.heavy = true
+		inst.sg.statemem.heavy_fast = inst:HasTag("mightiness_mighty")
+	elseif IsChannelCasting(inst) then
+		inst.sg.statemem.channelcast = true
+		inst.sg.statemem.channelcastitem = IsChannelCastingItem(inst)
+    elseif inst:HasTag("wereplayer") then
+        inst.sg.statemem.iswere = true
+        if inst:HasTag("weremoose") then
+            if inst:HasTag("groggy") then
+                inst.sg.statemem.moosegroggy = true
+            else
+                inst.sg.statemem.moose = true
+            end
+        elseif inst:HasTag("weregoose") then
+            if inst:HasTag("groggy") then
+                inst.sg.statemem.goosegroggy = true
+            else
+                inst.sg.statemem.goose = true
+            end
+        elseif inst:HasTag("groggy") then
+            inst.sg.statemem.groggy = true
+        else
+            inst.sg.statemem.normal = true
+        end
+	elseif inst:IsInAnyStormOrCloud() and not inst.components.playervision:HasGoggleVision() then
+        inst.sg.statemem.sandstorm = true
+    elseif inst:HasTag("groggy") then
+        inst.sg.statemem.groggy = true
+    elseif inst:IsCarefulWalking() then
+        inst.sg.statemem.careful = true
+    else
+        inst.sg.statemem.normal = true
+        inst.sg.statemem.normalwonkey = inst:HasTag("wonkey") or nil
+    end
+end
+
+
+local function GetRunStateAnim(inst)
+    return ((inst.sg.statemem.heavy and inst.sg.statemem.heavy_fast) and "heavy_walk_fast")
+        or (inst.sg.statemem.heavy and "heavy_walk")
+		or (inst.sg.statemem.channelcastitem and "channelcast_walk")
+		or (inst.sg.statemem.channelcast and "channelcast_oh_walk")
+        or (inst.sg.statemem.sandstorm and "sand_walk")
+        or ((inst.sg.statemem.groggy or inst.sg.statemem.moosegroggy or inst.sg.statemem.goosegroggy) and "idle_walk")
+        or (inst.sg.statemem.careful and "careful_walk")
+        or (inst.sg.statemem.ridingwoby and "run_woby")
+        or "run"
+end
+
+
+
+local function DoEquipmentFoleySounds(inst)
+    for k, v in pairs(inst.components.inventory.equipslots) do
+        if v.foleysound ~= nil then
+            inst.SoundEmitter:PlaySound(v.foleysound, nil, nil, true)
+        end
+    end
+end
+local function DoFoleySounds(inst)
+    DoEquipmentFoleySounds(inst)
+    if inst.foleysound ~= nil then
+        inst.SoundEmitter:PlaySound(inst.foleysound, nil, nil, true)
+    end
+end
+
+local function IsChannelCasting(inst)
+	--essentially prediction, since the actions aren't busy w/ lag states
+	local buffaction = inst.sg.mem.preview_channelcast_action
+	if buffaction then
+		return buffaction.action == ACTIONS.START_CHANNELCAST
+		--Don't use "or inst:IsChannelCasting()"
+		--We want to be able to return false here when predicting!
+	end
+	--otherwise return server state
+	return inst:IsChannelCasting()
+end
+
+
 --Client
 
 
-AddStategraphState("wilson", State {
+AddStategraphState("wilson_client", State {
     name = "run_welina_start",
     tags = { "moving", "running", "canrotate", "monkey" },
 
@@ -271,7 +395,7 @@ AddStategraphState("wilson", State {
     end,
 })
 
-AddStategraphState("wilson", State {
+AddStategraphState("wilson_client", State {
     name = "run_welina",
     tags = { "moving", "running", "canrotate", "monkey" },
 
@@ -393,8 +517,50 @@ local function SGWilsonPostInit(sg)
 
 end
 
+local function SGWilsonPostInit_CL(sg)
+	sg.states["run_start"].onenter = function(inst)
+		
+		
+        ConfigureRunState(inst)
+
+
+        if inst:HasTag("emocatgirl") and inst.components.locomotor:GetTimeMoving() >= TUNING.WONKEY_TIME_TO_RUN then
+            inst.sg:GoToState("run_welina") --resuming after brief stop from changing directions, or resuming prediction after running into obstacle
+            return
+        end
+        if inst.sg.statemem.normalwonkey and inst.components.locomotor:GetTimeMoving() >= TUNING.WONKEY_TIME_TO_RUN then
+            inst.sg:GoToState("run_monkey") --resuming after brief stop from changing directions, or resuming prediction after running into obstacle
+            return
+        end
+        inst.components.locomotor:RunForward()
+        inst.AnimState:PlayAnimation(GetRunStateAnim(inst).."_pre")
+        --goose footsteps should always be light
+        inst.sg.mem.footsteps = (inst.sg.statemem.goose or inst.sg.statemem.goosegroggy) and 4 or 0
+	
+    end
+
+
+    sg.states["run"].onupdate = function(inst)
+		
+        if inst:HasTag("emocatgirl") and  inst.components.locomotor:GetTimeMoving() >= TUNING.WONKEY_TIME_TO_RUN then
+            inst.sg:GoToState("run_welina_start")
+            return
+        end
+        if inst.sg.statemem.normalwonkey and inst.components.locomotor:GetTimeMoving() >= TUNING.WONKEY_TIME_TO_RUN then
+            inst.sg:GoToState("run_monkey_start")
+            return
+        end
+        inst.components.locomotor:RunForward()
+
+        
+	
+    end
+
+
+end
+
 AddStategraphPostInit("wilson", SGWilsonPostInit)
-AddStategraphPostInit("wilson_client", SGWilsonPostInit)
+AddStategraphPostInit("wilson_client", SGWilsonPostInit_CL)
 
 
 
