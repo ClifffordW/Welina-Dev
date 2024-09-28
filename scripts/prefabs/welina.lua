79825fd4572d8930b85a0e24a871238f6919bcfd
Cliffford W. <55302963+ClifffordW@users.noreply.github.com>
2024-09-28 03:51:56 +0200

    Update welina.lua

--- scripts/prefabs/welina.lua
+++ scripts/prefabs/welina.lua
@@ -361,6 +361,7 @@ local function OnEat(inst, food)
 
         inst.welina_eatenitem = food.prefab
         inst:DoTaskInTime(math.random(2,5), function()
+            if inst.sg:HasStateTag("dead") or inst:HasTag("playerghost") then return end
             inst.sg:GoToState("welina_vomit_pre")
 
         end)
